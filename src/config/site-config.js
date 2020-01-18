8d45c0a3d7c8a47bbcbde092c34541a192d8002c
Hong Kun Tian <hongkun.tian@hotmail.com>
2020-01-18 17:33:56 -0500

    Initial commit

---
+++ src/config/site-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    siteTitle: 'I\'m Hong Kun Tian',
+    siteDescription: 'Honours Computer Science Student at McGill University.',
+    firstName = 'Hong Kun',
+    lastName = 'Tian',
+    email = 'hongkun.tian@hotmail.com',
+    socialMedia: [
+        {
+            name: 'GitHub',
+            url: 'https://github.com/hongkuntian'
+        },
+        {
+            name: 'LinkedIn',
+            url: 'https://linkedin.com/in/hongkuntian'
+        },
+        {
+            name: 'DevPost',
+            url: 'https://devpost.com/'
+        }
+    ],
+    navLinks: [
+        {
+          name: 'About',
+          url: '/#about',
+        },
+        {
+          name: 'Experience',
+          url: '/#jobs',
+        },
+        {
+          name: 'Work',
+          url: '/#projects',
+        },
+        {
+          name: 'Contact',
+          url: '/#contact',
+        },
+      ],
+      //TODO: Add light/dark theme colours here too
+}
