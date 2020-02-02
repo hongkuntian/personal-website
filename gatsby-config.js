9dab20741761e02872a971e2f246d322861f4d2d
Hong Kun Tian <hongkun.tian@hotmail.com>
2020-02-02 13:51:41 -0500

    Add more responsiveness to header

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || `none`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -5,13 +5,15 @@ import "../../styles/hero.scss"
 const Hero = () => (
   <div className="container container-full vertical-center">
     <div className="landing">
-      <p className="greetings">Hello there, I'm</p>
-      <h1>
-        <span>Hong Kun</span> <span>Tian</span>
-      </h1>
-      <p>Software Developer</p>
-      <p>Mind your step! Site is still under construction...</p>
-    </div>
+      <div className="landing-content-container">
+        <p className="greetings">Hello there, I'm</p>
+        <h1>
+          <span>Hong Kun</span> <span>Tian</span>
+        </h1>
+        <p>Software Developer</p>
+        <p>Mind your step! Site is still under construction...</p>
+      </div>
+      </div>
   </div>
 )
 
