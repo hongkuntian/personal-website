adeeaef22d8583e3b43ded45699bac815ce4c019
Hong Kun Tian <hongkun.tian@hotmail.com>
2020-02-29 19:35:42 -0500

    Update about me paragraph

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -20,15 +20,20 @@ const About = ({ data }) => (
         </div>
         <div className="about-text">
           <p>
-            I'm currently enrolled in <a href="https://www.mcgill.ca" className="un">McGill University</a>'s Honours Computer Science program
-            and have a cumulative GPA of 3.64/4.00. <Emoji symbol="✨" /> I've
-            been introduced to programming in high school and have ever since
-            fallen in love with it. I'm super analytical and detail-oriented, so
-            it's pretty much a match made in heaven! I haven't set my sights on
-            a particular field yet since all the options seem so interesting and
-            I'd like to explore them all. My philosophy in life is to leave the
-            world better than we found it, and every step I take is to further
-            that personal mantra in order to make the world slightly better.{" "}
+            I'm currently enrolled in{" "}
+            <a href="https://www.mcgill.ca" className="un">
+              McGill University
+            </a>
+            's Honours Computer Science program and have a cumulative GPA of
+            3.64/4.00. <Emoji symbol="✨" /> I've been introduced to programming
+            in high school and have ever since fallen in love with it. I'm super
+            analytical and detail-oriented, so it's pretty much a match made in
+            heaven!
+            <p></p>The world of machine learning, cloud computing, and
+            distributed systems are a few of the many fields that have piqued my
+            interest. In our world of ever emerging new technologies, I view
+            learning as a never ending background task that propels me forward
+            at every step of the way.{" "}
             <Emoji symbol="😁" />
           </p>
         </div>
@@ -37,19 +42,68 @@ const About = ({ data }) => (
         <div className="courses-container">
           <h3>A few notable courses I've completed</h3>
           <ul>
-            <li><a href="https://www.mcgill.ca/study/2018-2019/courses/comp-302" className="un">Functional Programming</a></li>
-            <li><a href="https://www.mcgill.ca/study/2019-2020/courses/comp-310" className="un">Operating Systems</a></li>
-            <li><a href="https://www.mcgill.ca/study/2019-2020/courses/comp-350" className="un">Numerical Computing</a></li>
-            <li><a href="https://www.mcgill.ca/study/2019-2020/courses/comp-557" className="un">Fundamentals of Computer Graphics</a></li>
+            <li>
+              <a
+                href="https://www.mcgill.ca/study/2018-2019/courses/comp-302"
+                className="un"
+              >
+                Functional Programming
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.mcgill.ca/study/2019-2020/courses/comp-310"
+                className="un"
+              >
+                Operating Systems
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.mcgill.ca/study/2019-2020/courses/comp-350"
+                className="un"
+              >
+                Numerical Computing
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.mcgill.ca/study/2019-2020/courses/comp-557"
+                className="un"
+              >
+                Fundamentals of Computer Graphics
+              </a>
+            </li>
           </ul>
         </div>
         <div className="courses-container">
-        	<h3>Some courses I'm currently taking</h3>
-        	<ul>
-        	  <li><a href="https://www.mcgill.ca/study/2019-2020/courses/comp-303" className="un">Software Design</a></li>
-        	  <li><a href="https://www.mcgill.ca/study/2019-2020/courses/comp-421" className="un">Database Systems</a></li>
-        	  <li><a href="https://www.mcgill.ca/study/2019-2020/courses/comp-551" className="un">Applied Machine Learning</a></li>
-        	</ul>
+          <h3>Some courses I'm currently taking</h3>
+          <ul>
+            <li>
+              <a
+                href="https://www.mcgill.ca/study/2019-2020/courses/comp-303"
+                className="un"
+              >
+                Software Design
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.mcgill.ca/study/2019-2020/courses/comp-421"
+                className="un"
+              >
+                Database Systems
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.mcgill.ca/study/2019-2020/courses/comp-551"
+                className="un"
+              >
+                Applied Machine Learning
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
