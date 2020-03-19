155f2d806a4adf73e4c6fe47150caeaf0842b95b
Hong Kun Tian <hongkun.tian@hotmail.com>
2020-03-18 21:24:19 -0400

    Update footer copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,6 +11,7 @@ const Footer = () => (
   <footer>
     <div className="footer-container">
       <div><small>Copyright &copy; {currYear()} Hong Kun Tian.&nbsp;</small></div>
+      <small>All rights reversed.&nbsp;</small>
       <a href="https://github.com/hongkuntian/personal-website" className="un">
         <small>Built with Gatsby</small>    
       </a>
