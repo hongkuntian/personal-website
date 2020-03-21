9e096e19756f4738c953729af9d57a27ab805046
Hong Kun Tian <hongkun.tian@hotmail.com>
2020-03-21 18:27:22 -0400

    Update Experience section

--- src/components/sections/experience.js
+++ src/components/sections/experience.js
@@ -14,7 +14,7 @@ const Experience = () => (
         <h3>Software Development Intern</h3>
         <p>June 2019 — August 2019</p>
         <ul>
-          <li>Developed a RESTful API for <a href="https://cloudockit.com" className="un">Cloudockit</a> using ASP.NET Framework</li>
+          <li>Developed a server-side REST API for <a href="https://cloudockit.com" className="un">Cloudockit</a>, a cloud environment diagram generator for AWS, Azure and GCP, using .NET Framework, through which end consumers can trigger document generation.</li>
           <li>Built an interactive page using HTML, CSS, and Javascript through which end consumers can visualize and interact with the API’s endpoints</li>
           <li>Developed test cases that detected irregularities and unwanted changes within template documents and generated documents using Aspose file management APIs</li>
         </ul>
