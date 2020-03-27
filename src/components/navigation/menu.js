391b1ed1ccacd261a90d7c073e2ffbeb28c4e9ff
Hong Kun Tian <hongkun.tian@hotmail.com>
2020-03-26 20:06:18 -0400

    Add scrollspy

--- src/components/navigation/menu.js
+++ src/components/navigation/menu.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Item from "./item.js"
+import Scrollspy from "react-scrollspy"
 
 import "../../styles/menu.scss"
 
@@ -12,17 +13,47 @@ const Menu = props => {
   return (
     <div className={menuClasses}>
       <nav>
-        <Item closeCallback={props.closeCallback} name="About" id="about" className="anchor" />
-        <Item closeCallback={props.closeCallback} name="Experience" id="experience" className="anchor" />
-        <Item closeCallback={props.closeCallback} name="Projects" id="projects" className="anchor" />
-        <Item closeCallback={props.closeCallback} name="Contact" id="contact" className="anchor" />
+        <Scrollspy items={["about", "experience", "projects", "contact"]} currentClassName="current-nav-item">
+          <li>
+            <Item
+              closeCallback={props.closeCallback}
+              name="About"
+              id="about"
+              className="anchor"
+            />
+          </li>
+          <li>
+            <Item
+              closeCallback={props.closeCallback}
+              name="Experience"
+              id="experience"
+              className="anchor"
+            />
+          </li>
+          <li>
+            <Item
+              closeCallback={props.closeCallback}
+              name="Projects"
+              id="projects"
+              className="anchor"
+            />
+          </li>
+          <li>
+            <Item
+              closeCallback={props.closeCallback}
+              name="Contact"
+              id="contact"
+              className="anchor"
+            />
+          </li>
+        </Scrollspy>
       </nav>
     </div>
   )
 }
 
 Menu.propTypes = {
-  dir: PropTypes.string.isRequired
+  dir: PropTypes.string.isRequired,
 }
 
 export default Menu
