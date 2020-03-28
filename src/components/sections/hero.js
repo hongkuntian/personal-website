7fb929f9ed9e5eb520dd71384aab0aa75efd6366
Hong Kun Tian <hongkun.tian@hotmail.com>
2020-03-28 13:20:30 -0400

    Fix landing responsive styling and header

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -5,6 +5,9 @@ import "../../styles/hero.scss"
 const Hero = () => (
   <div className="vertical-center container-full">
     <div className="landing-outer-container landing-outer-container-light ">
+      <div className="bg-name">
+        <p>HKT</p>
+      </div>
       <div className="landing-container">
         <div className="landing">
           <div className="landing-content-container">
@@ -24,11 +27,19 @@ const Hero = () => (
         <a class="scroll-clickable" href="#about">
           <div class="scroll-down"></div>
         </a>
-        <div className="bg-name">
-          <p>HKT</p>
-        </div>
       </div>
     </div>
+    {/* <div className="">
+      <div className="landing-container">
+        <div className="landing">
+          <div className="landing-content-container">
+            <div className="bg-name">
+              <p>HKT</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> */}
   </div>
 )
 
